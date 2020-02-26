49320bf3c07aa291a6eef043447354771140a4ea
dlstonedl <ldeng@thoughtworks.com>
2020-02-26 15:46:28 +0800

    rename package

--- src/main/java/com/dlstone/dynamic/scheduler/DynamicSchedulerApplication.java
+++ src/main/java/com/dlstone/dynamic/scheduler/DynamicSchedulerApplication.java
@@ -1,4 +1,4 @@
-package com.dlstone.dynamicscheduler;
+package com.dlstone.dynamic.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
