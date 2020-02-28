c22b99659ba34aee486eb90cdfbc39ff519a0fb3
dlstonedl <ldeng@thoughtworks.com>
2020-02-28 09:14:40 +0800

    add job

--- src/main/java/com/dlstone/dynamic/scheduler/service/QuartzService.java
+++ src/main/java/com/dlstone/dynamic/scheduler/service/QuartzService.java
@@ -1,16 +1,20 @@
 package com.dlstone.dynamic.scheduler.service;
 
+import com.dlstone.dynamic.scheduler.configuration.JobTemplate;
 import com.dlstone.dynamic.scheduler.model.SchedulerTask;
 import com.dlstone.dynamic.scheduler.model.SchedulerTaskFactory;
 import com.dlstone.dynamic.scheduler.wrapper.SchedulerWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class QuartzService {
 
@@ -35,5 +39,43 @@ public class QuartzService {
             .stream()
             .map(trigger -> SchedulerTaskFactory.generateSchedulerTask(schedulerWrapper, jobDetail, trigger))
             .collect(Collectors.toList());
+    }
+
+    public void addJob(SchedulerTask schedulerTask) {
+        JobDetail jobDetail = newJobDetail(schedulerTask, schedulerWrapper);
+        CronTrigger cronTrigger = newCronTrigger(schedulerTask, jobDetail);
+        schedulerWrapper.scheduleJob(jobDetail, cronTrigger);
+    }
+
+    private CronTrigger newCronTrigger(SchedulerTask schedulerTask, JobDetail jobDetail) {
+        return TriggerBuilder.newTrigger()
+            .forJob(jobDetail)
+            .withDescription(LocalDateTime.now().toString())
+            .withIdentity(schedulerTask.getJobTriggerName(), schedulerTask.getJobTriggerGroupName())
+            .withSchedule(CronScheduleBuilder
+                .cronSchedule(schedulerTask.getJobTriggerCronExpression())
+                .withMisfireHandlingInstructionDoNothing())
+            .build();
+    }
+
+    private JobDetail newJobDetail(SchedulerTask schedulerTask, SchedulerWrapper schedulerWrapper) {
+        JobKey jobKey = newJobKey(schedulerTask, schedulerWrapper);
+
+        return JobBuilder.newJob()
+            .ofType(JobTemplate.class)
+            .storeDurably()
+            .withIdentity(jobKey)
+            .withDescription(schedulerTask.getJobDescription())
+            .usingJobData(schedulerTask.getJobDataMap())
+            .build();
+    }
+
+    private JobKey newJobKey(SchedulerTask schedulerTask, SchedulerWrapper schedulerWrapper) {
+        JobKey jobKey = new JobKey(schedulerTask.getJobName(), schedulerTask.getJobGroup());
+        if (schedulerWrapper.checkExists(jobKey)) {
+            log.error("jobKey exist, {}", jobKey);
+            throw new RuntimeException(String.format("job exist, jobName: %s, jobGroupName: %s", jobKey.getName(), jobKey.getGroup()));
+        }
+        return jobKey;
     }
 }
