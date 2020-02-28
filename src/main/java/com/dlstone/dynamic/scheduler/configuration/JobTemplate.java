fbe7cbebe8869b1281ac72a5b241f8c28806cb80
dlstonedl <ldeng@thoughtworks.com>
2020-02-28 09:13:43 +0800

    job template

---
+++ src/main/java/com/dlstone/dynamic/scheduler/configuration/JobTemplate.java
@@ -0,0 +1,24 @@
+package com.dlstone.dynamic.scheduler.configuration;
+
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+public class JobTemplate extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap mergedJobDataMap = context.getMergedJobDataMap();
+        String url = mergedJobDataMap.getString("url");
+
+        RestTemplate restTemplate = new RestTemplateBuilder()
+            .uriTemplateHandler(new DefaultUriBuilderFactory())
+            .build();
+        restTemplate.postForObject(url, null, Object.class);
+    }
+
+}
