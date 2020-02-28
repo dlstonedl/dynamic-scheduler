2860c975d79479cfd8f3c052f7bd3b124b6d5f39
dlstonedl <ldeng@thoughtworks.com>
2020-02-28 11:42:21 +0800

    quartz validator

--- src/main/java/com/dlstone/dynamic/scheduler/controller/request/JobRequest.java
+++ src/main/java/com/dlstone/dynamic/scheduler/controller/request/JobRequest.java
@@ -1,9 +1,14 @@
 package com.dlstone.dynamic.scheduler.controller.request;
 
+import javax.validation.constraints.NotNull;
+
 public class JobRequest {
+    @NotNull
     public String jobName;
     public String jobGroupName;
     public String jobDescription;
+    @NotNull
     public String url;
+    @NotNull
     public String cronExpression;
 }
--- src/main/java/com/dlstone/dynamic/scheduler/controller/request/UpdateJobRequest.java
+++ src/main/java/com/dlstone/dynamic/scheduler/controller/request/UpdateJobRequest.java
@@ -1,7 +1,11 @@
 package com.dlstone.dynamic.scheduler.controller.request;
 
+import javax.validation.constraints.NotNull;
+
 public class UpdateJobRequest {
     public String jobDescription;
+    @NotNull
     public String url;
+    @NotNull
     public String cronExpression;
 }
