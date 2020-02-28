47e9391286c36f982317324c86c786c475a8e0d1
dlstonedl <ldeng@thoughtworks.com>
2020-02-29 02:28:34 +0800

    repair mybatis LocalDateTimeTypeHandler

---
+++ src/main/java/com/dlstone/dynamic/scheduler/configuration/LocalDateTimeTypeHandler.java
@@ -0,0 +1,38 @@
+package com.dlstone.dynamic.scheduler.configuration;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeTypeHandler extends BaseTypeHandler<LocalDateTime> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, LocalDateTime parameter, JdbcType jdbcType) throws SQLException {
+        ps.setObject(i, parameter.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        Timestamp timestamp = rs.getTimestamp(columnName);
+        return getLocalDateTime(timestamp);
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        Timestamp timestamp = rs.getTimestamp(columnIndex);
+        return getLocalDateTime(timestamp);
+    }
+
+    @Override
+    public LocalDateTime getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        Timestamp timestamp = cs.getTimestamp(columnIndex);
+        return getLocalDateTime(timestamp);
+    }
+
+    private static LocalDateTime getLocalDateTime(Timestamp timestamp) {
+        return timestamp == null ? null : timestamp.toLocalDateTime();
+    }
+}
