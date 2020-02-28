3364d9d3dabd1d5c41dcc7ef2f2be0d82792ed24
dlstonedl <ldeng@thoughtworks.com>
2020-02-29 00:51:18 +0800

    job history

---
+++ src/main/resources/db/migration/V20200228230610__create-quartz-history-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE job_history(
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  job_name VARCHAR(190) NOT NULL,
+  job_group VARCHAR(190) NOT NULL,
+  trigger_name VARCHAR(190) NOT NULL,
+  trigger_group VARCHAR(190) NOT NULL,
+  job_start_time date,
+  job_end_time date,
+  job_duration BIGINT(13),
+  job_status VARCHAR(10),
+  job_exception VARCHAR(8000),
+  PRIMARY KEY (ID)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
